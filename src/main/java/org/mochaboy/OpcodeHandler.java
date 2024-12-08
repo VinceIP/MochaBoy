77761cef6552770497812e2debf622959d73492d
VinceIP <vinceip532@gmail.com>
2024-12-08 17:32:28 -0500

    quick end of day commit

--- src/main/java/org/mochaboy/OpcodeHandler.java
+++ src/main/java/org/mochaboy/OpcodeHandler.java
@@ -5,14 +5,34 @@ import java.util.Map;
 public class OpcodeHandler {
     private Map<Integer, OpcodeInfo> opcodeMap;
 
-    public OpcodeHandler(Map<Integer,OpcodeInfo> opcodeMap){
+    public OpcodeHandler(Map<Integer, OpcodeInfo> opcodeMap) {
         this.opcodeMap = opcodeMap;
     }
 
-    public void execute(OpcodeInfo opcode){
+    public void execute(CPU cpu, OpcodeInfo opcode) {
         //do logic on cpu
-        //inc PC
-        //add to cpu timer
+        //inc PC based on opcode info
+        //add to cpu timer based on opcode info
+        //consider increasing t states during specific operations (reading and writing) for accuracy instead?
     }
+
+    /**
+     * Add carry - Add value of y plus carry flag to x
+     *
+     * @param cpu
+     * @param opcode
+     */
+    private void ADC(CPU cpu, OpcodeInfo opcode) {
+        //No need to get an xOpr - ADC's x operand is always A, so we alays know where to get the value
+        Operand yOpr = opcode.getOperands()[1];
+        int xVal = cpu.getRegisters().getA() & 0xFF;
+        int yVal;
+        if(yOpr.getName().equals("n8")) yVal = cpu.getRegisters().getPC();
+        else yVal = cpu.getRegisters().getByName(yOpr.getName()) & 0xFF;
+        int result = yVal + xVal & 0xFF;
+        cpu.getRegisters().setA(result);
+    }
+
+
 }
 
--- src/main/java/org/mochaboy/Registers.java
+++ src/main/java/org/mochaboy/Registers.java
@@ -1,16 +1,35 @@
 package org.mochaboy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class Registers {
 
-    private int A,B,C,D,E,F,H,L;
+    private final Map<String, Supplier<Integer>> registerMap; //Will be needed to dynamically access registers as referenced by OpcodeInfo
+    private int A, B, C, D, E, F, H, L;
     private int SP, PC;
     public static final int FLAG_ZERO = 1 << 7;
     public static final int FLAG_SUBTRACT = 1 << 6;
     public static final int FLAG_HALF_CARRY = 1 << 5;
     public static final int FLAG_CARRY = 1 << 4;
 
-    public Registers(){
-        setPC(0x00);
+    public Registers() {
+        registerMap = new HashMap<>();
+        registerMap.put("A", () -> A);
+        registerMap.put("B", () -> B);
+        registerMap.put("C", () -> C);
+        registerMap.put("D", () -> D);
+        registerMap.put("E", () -> E);
+        registerMap.put("F", () -> F);
+        registerMap.put("H", () -> H);
+        registerMap.put("L", () -> L);
+        registerMap.put("SP", () -> SP);
+        registerMap.put("PC", () -> PC);
+        registerMap.put("AF", this::getAF);
+        registerMap.put("BC", this::getBC);
+        registerMap.put("DE", this::getDE);
+        registerMap.put("HL", this::getHL);
     }
 
     public int getA() {
@@ -93,63 +112,65 @@ public class Registers {
         this.PC = PC;
     }
 
-    public void incrementPC(){
+    public void incrementPC() {
         setPC(getPC() + 1);
     }
 
-    public void incrementPC(int val){
+    public void incrementPC(int val) {
         setPC(getPC() + val);
     }
 
     //16-bit combined registers
-    public int getAF(){ //Accumulator and flags
+    public int getAF() { //Accumulator and flags
         return (A << 8) | F;
     }
 
-    public void setAF(){
+    public void setAF() {
         System.out.println("setAF unimplemented");
     }
 
-    public int getBC(){
+    public int getBC() {
         return (B << 8) | C;
     }
 
-    public void setBC(int val){
+    public void setBC(int val) {
         B = (val >> 8) & 0xFF;
         C = val & 0xFF;
     }
 
-    public int getDE(){
+    public int getDE() {
         return (D << 8) | E;
     }
 
-    public void setDE(int val){
+    public void setDE(int val) {
         D = (val >> 8) & 0xFF;
         E = val & 0xFF;
     }
 
-    public int getHL(){
+    public int getHL() {
         return (H << 8) | L;
     }
 
-    public void setHL(int val){
+    public void setHL(int val) {
         H = (val >> 8) & 0xFF;
         L = val & 0xFF;
     }
 
     //Flags
 
-    public boolean isFlagSet(int flag){
+    public boolean isFlagSet(int flag) {
         return (F & flag) != 0;
     }
 
-    public void setFlag(int flag){
+    public void setFlag(int flag) {
         F |= flag;
     }
 
-    public void clearFlag(int flag){
+    public void clearFlag(int flag) {
         F &= ~flag;
     }
 
-
+    public int getByName(String name) {
+        return registerMap.get(name).get();
+    }
 }
